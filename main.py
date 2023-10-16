f2937be520f9817ee9a21afbbd87caa2e6972481
Jerome B. <77253921+keeliman@users.noreply.github.com>
2023-10-16 17:06:10 +0200

    English

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from dotenv import load_dotenv
 
-# Charger les variables du fichier .env
+# Load variables from .env file
 load_dotenv()
 API_KEY = os.getenv('API_KEY')
 PREFIX = os.getenv('PREFIX')
