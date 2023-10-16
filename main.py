4405b05b89a0dad5f58c0b0af15518a31379aa09
Jerome B. <77253921+keeliman@users.noreply.github.com>
2023-10-16 19:46:27 +0200

    Refactor

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+import os
+import pickle
 import datetime
 import random
-import os
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
 from dotenv import load_dotenv
+
 
 # ---------------- Configuration Loading ----------------
 
@@ -15,13 +19,33 @@ def load_configurations():
         "SUFFIX": os.getenv('SUFFIX'),
         "DESCRIPTION": os.getenv('DESCRIPTION').replace('\\n', '\n'),
         "FIRST_INTERVAL": (int(os.getenv('FIRST_INTERVAL_START')), int(os.getenv('FIRST_INTERVAL_END'))),
-        "SECOND_INTERVAL": (int(os.getenv('SECOND_INTERVAL_START')), int(os.getenv('SECOND_INTERVAL_END')))
+        "SECOND_INTERVAL": (int(os.getenv('SECOND_INTERVAL_START')), int(os.getenv('SECOND_INTERVAL_END'))),
+        "MAX_VIDEOS": int(os.getenv('MAX_VIDEOS', 50)),  # Default to 50 if not set
+        "START_DATE": datetime.datetime.strptime(os.getenv('START_DATE', datetime.datetime.now().strftime('%Y-%m-%d')), '%Y-%m-%d')
     }
 
-# ---------------- YouTube API Interactions ----------------
 
-def initialize_youtube(api_key):
-    return build('youtube', 'v3', developerKey=api_key)
+
+# ---------------- OAuth Authentication ----------------
+
+def authenticate_with_oauth():
+    creds = None
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', ['https://www.googleapis.com/auth/youtube.force-ssl'])
+            creds = flow.run_local_server(port=0)
+            with open('token.pickle', 'wb') as token:
+                pickle.dump(creds, token)
+    
+    return build('youtube', 'v3', credentials=creds)
+
+# ---------------- YouTube API Interactions ----------------
 
 def get_draft_videos(youtube):
     request = youtube.videos().list(part="snippet,status", mine=True, maxResults=50)
@@ -62,11 +86,11 @@ def calculate_publish_time(start_date, index, first_interval, second_interval):
 # ---------------- Main Execution ----------------
 
 def main():
+    youtube = authenticate_with_oauth()
     config = load_configurations()
-    youtube = initialize_youtube(config["API_KEY"])
     
-    draft_videos = get_draft_videos(youtube)
-    start_date = datetime.datetime.now()
+    draft_videos = get_draft_videos(youtube)[:config["MAX_VIDEOS"]]  # Limit the number of videos
+    start_date = config["START_DATE"]
     
     for i, video in enumerate(draft_videos):
         title = process_video_title(video, config["PREFIX"], config["SUFFIX"])
@@ -75,7 +99,7 @@ def main():
             update_video(youtube, video, title, config["DESCRIPTION"], publish_time)
             print(f"Updated video {video['snippet']['title']} for publishing at {publish_time}")
         except HttpError as e:
-            print(f"An error occurred: {e}")
+            print(f"An error occurred while updating video {video['snippet']['title']}: {e}")
 
 if __name__ == "__main__":
     main()
